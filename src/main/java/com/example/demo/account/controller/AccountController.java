69f269ec789a8072e480ac223d264df478969620
Hakgyu Kim <zage4448@gmail.com>
2023-07-05 11:35:02 +0900

    feat: [김학규] 사용자가 로그인 할 수 있다 [TGS-7]

--- src/main/java/com/example/demo/account/controller/AccountController.java
+++ src/main/java/com/example/demo/account/controller/AccountController.java
@@ -1,11 +1,14 @@
 package com.example.demo.account.controller;
 
+import com.example.demo.account.controller.form.AccountLoginRequestForm;
 import com.example.demo.account.controller.form.AccountRegisterForm;
 import com.example.demo.account.service.AccountService;
 import com.example.demo.redis.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
 
     final private AccountService accountService;
+    final private RedisService redisService;
 
     @PostMapping("/create-account")
     public Boolean accountRegister (@RequestBody AccountRegisterForm registerForm) {
@@ -23,5 +27,18 @@ public class AccountController {
     @GetMapping("/check-email-duplicate/{email}")
     public Boolean checkEmail(@PathVariable("email") String email) {
         return accountService.checkDuplicateEmail(email);
+    }
+
+    @PostMapping("/login")
+    public String accountLogin (@RequestBody AccountLoginRequestForm requestForm) {
+        Long accountId = accountService.login(requestForm);
+        if (accountId != null) {
+            UUID userToken = UUID.randomUUID();
+
+            redisService.setKeyAndValue(userToken.toString(), accountId);
+
+            return userToken.toString();
+        }
+        return null;
     }
 }

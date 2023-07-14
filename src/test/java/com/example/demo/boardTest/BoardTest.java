88ebe16ea7cf04d5cfcc102e02709e09f2d48ddf
Hakgyu Kim <zage4448@gmail.com>
2023-07-14 14:03:27 +0900

    feat: [김학규] 기존 board test 코드 mocking 으로 수정 [TGS-25]

--- src/test/java/com/example/demo/boardTest/BoardTest.java
+++ src/test/java/com/example/demo/boardTest/BoardTest.java
@@ -62,67 +62,123 @@ public class BoardTest {
 //
 //    @Autowired
 //    private BoardService boardService;
+//
+//    @Test
+//    @DisplayName("게시물 등록 테스트")
+//    void registerBoardTest () {
+//        final Long accountId = 1l;
+//        final String title = "test board title";
+//        final String content = "test board content";
+//        final BoardCategory category = Asia;
+//
+//        Account account = accountService.findAccountById(accountId);
+//
+//
+//        BoardRegisterRequest registerRequest = new BoardRegisterRequest(title, account, content, category);
+//
+//        Long boardId = boardService.register(registerRequest);
+//        ReadBoardResponseForm board = boardService.read(boardId);
+//        assertEquals(title, board.getTitle());
+//        assertEquals(content, board.getContent());
+//        assertEquals(account.getNickname(), board.getWriter());
+//        assertEquals(category, board.getBoardCategory());
+//    }
+//
+//    @Test
+//    @DisplayName("게시글 불러오기 테스트")
+//    void readBoardTest () {
+//        final Long boardId = 1l;
+//        final String title = "test board title";
+//        final String content = "test board content";
+//
+//        ReadBoardResponseForm board = boardService.read(boardId);
+//
+//        assertEquals(title, board.getTitle());
+//        assertEquals(content, board.getContent());
+//    }
+//
+//    @Test
+//    @DisplayName("카테고리별 게시글 리스트 불러오기")
+//    void bringCategorizedBoardList () {
+//        final BoardCategory category = Europe;
+//        final String title = "test board title";
+//        final Long boardId = 1l;
+//
+//        List<CategoryBoardListResponseForm> responseFormList = boardService.getListByCategory(category);
+//        CategoryBoardListResponseForm responseForm = responseFormList.get(0);
+//
+//        assertEquals(title, responseForm.getTitle());
+//        assertEquals(boardId, responseForm.getBoardId());
+//    }
+//
+//    @Test
+//    @DisplayName("카테고리와 카테고리별 게시글 수량 리스트 불러오기")
+//    void bringCategoryListTest() {
+//        final BoardCategory category = Europe;
+//        final Long posts = 1l;
+//
+//        List<CategoryListForm> categoryList = boardService.getCategoryList();
+//
+//        BoardCategory actualCategory = categoryList.get(1).getBoardCategory();
+//        Long actualPosts = categoryList.get(1).getPosts();
+//        assertEquals(category, actualCategory);
+//        assertEquals(posts, actualPosts);
+//    }
 
     @Test
     @DisplayName("게시물 등록 테스트")
-    void registerBoardTest () {
-        final Long accountId = 1l;
+    void registerBoardTest() {
+        final Long boardId = 1L;
         final String title = "test board title";
         final String content = "test board content";
-        final BoardCategory category = Asia;
+        final BoardCategory category = BoardCategory.Asia;
 
-        Account account = accountService.findAccountById(accountId);
-
+        Account account = new Account("test@test.com", "password", "tester");
 
         BoardRegisterRequest registerRequest = new BoardRegisterRequest(title, account, content, category);
 
-        Long boardId = boardService.register(registerRequest);
-        ReadBoardResponseForm board = boardService.read(boardId);
-        assertEquals(title, board.getTitle());
-        assertEquals(content, board.getContent());
-        assertEquals(account.getNickname(), board.getWriter());
-        assertEquals(category, board.getBoardCategory());
+        Mockito.when(boardRepository.save(Mockito.any(Board.class))).thenAnswer(invocation -> {
+            Board savedBoard = invocation.getArgument(0);
+            Field field = savedBoard.getClass().getDeclaredField("boardId");
+            field.setAccessible(true);
+            field.set(savedBoard, boardId);
+            return savedBoard;
+        });
+
+        Long actual = boardService.register(registerRequest);
+
+        assertEquals(boardId, actual);
     }
 
     @Test
     @DisplayName("게시글 불러오기 테스트")
-    void readBoardTest () {
-        final Long boardId = 1l;
+    void readBoardTest() throws NoSuchFieldException, IllegalAccessException {
+        Account account = new Account("test@test.com", "password", "tester");
         final String title = "test board title";
         final String content = "test board content";
+        LocalDateTime now = LocalDateTime.now();
 
-        ReadBoardResponseForm board = boardService.read(boardId);
+        Board board = createBoardWithDate(title, account, content, Asia, now, now);
 
-        assertEquals(title, board.getTitle());
-        assertEquals(content, board.getContent());
+
+        Mockito.when(boardRepository.findById(0L)).thenReturn(Optional.of(board));
+
+        ReadBoardResponseForm actual = boardService.read(0L);
+
+        assertEquals(board.getTitle(), actual.getTitle());
+        assertEquals(content, actual.getContent());
     }
 
     @Test
     @DisplayName("카테고리별 게시글 리스트 불러오기")
-    void bringCategorizedBoardList () {
-        final BoardCategory category = Europe;
-        final String title = "test board title";
-        final Long boardId = 1l;
+    void bringCategorizedBoardList() {
+        final BoardCategory category = BoardCategory.Europe;
+
+        Mockito.when(boardRepository.findAllByCategory(category)).thenReturn(Collections.emptyList());
 
         List<CategoryBoardListResponseForm> responseFormList = boardService.getListByCategory(category);
-        CategoryBoardListResponseForm responseForm = responseFormList.get(0);
 
-        assertEquals(title, responseForm.getTitle());
-        assertEquals(boardId, responseForm.getBoardId());
-    }
-
-    @Test
-    @DisplayName("카테고리와 카테고리별 게시글 수량 리스트 불러오기")
-    void bringCategoryListTest() {
-        final BoardCategory category = Europe;
-        final Long posts = 1l;
-
-        List<CategoryListForm> categoryList = boardService.getCategoryList();
-
-        BoardCategory actualCategory = categoryList.get(1).getBoardCategory();
-        Long actualPosts = categoryList.get(1).getPosts();
-        assertEquals(category, actualCategory);
-        assertEquals(posts, actualPosts);
+        assertTrue(responseFormList.isEmpty());
     }
 
 
